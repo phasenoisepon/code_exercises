bca3d19ab81275412f60255d8dc25087dfb1600b
phasenoisepon <82134074+phasenoisepon@users.noreply.github.com>
2021-08-19 18:13:54 -0500

    initial commit

---
+++ merge_sorted_linked_list/linkedlist.py
@@ -0,0 +1,44 @@
+"""
+Given two sorted linked lists, merge them so that the resulting linked list is also sorted.
+"""
+
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class SingleLinkedList(object):
+    """
+    singly linked list implementation
+    """
+
+    def __init__(self):
+        self.head = None
+    pass
+
+    def create_from_list(self, input_list: list):
+        assert self.head is None
+        if len(input_list) < 1:
+            return
+        if len(input_list) == 1:
+            self.head = Node(input_list[0])
+            return
+
+        current = self.head
+        for item in input_list:
+            if self.head is None:
+                self.head = Node(item)
+            else:
+                current.next = Node(item)
+                current = current.next
+
+
+def merge_sorted(list1: SingleLinkedList, list2: SingleLinkedList) -> SingleLinkedList:
+
+    return SingleLinkedList()
+
+
+if __name__ == "__main__":
+    pass
---
+++ tests/merge_sorted_linked_list/test_linkedlist.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestSingleLinkedList(TestCase):
+    def test_create_from_list(self):
+        self.fail()
