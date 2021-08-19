c40afacd4a957695f20a7e108e3e9ebb7ec8d02b
phasenoisepon <82134074+phasenoisepon@users.noreply.github.com>
2021-08-19 18:18:22 -0500

    test cases

--- tests/merge_sorted_linked_list/test_linkedlist.py
+++ tests/merge_sorted_linked_list/test_linkedlist.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
-
+from merge_sorted_linked_list import SingleLinkedList
 
 class TestSingleLinkedList(TestCase):
-    def test_create_from_list(self):
-        self.fail()
+    def test_create_from_list_single(self):
+        self.assertEqual(list(1),)
+    def test_create_from_list_double(self):
+    def test_create_from_list_triple(self):
