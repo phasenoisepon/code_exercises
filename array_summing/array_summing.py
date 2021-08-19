caf723e12d9c9729125acce1e6e9fe47f4988ee8
phasenoisepon <82134074+phasenoisepon@users.noreply.github.com>
2021-08-18 23:16:30 -0500

    sum two numbers and test

--- array_summing/array_summing.py
+++ array_summing/array_summing.py
@@ -8,28 +8,36 @@ Target 10: 7+3, 2+8
 Target 19: No 2 values sum
 """
 
-given = [5, 7, 1, 2, 7, 4, 3]
-search = 10
 
-def sum_two_target(given: list, search: int)->bool:
-    # determine if there are any two = return early when a single match is found
-    # algorithm: add values to a set, then check while searching given list if target - current exists in set
-    my_set = set(given)
-    for element in given:
-        if search - element in my_set:
-            return True
-    return False
-
-def sum_two_target_naive(given: list, search: int)->bool:
-    # determine if there are any two = return early when a single match is found
-    # algorithm: check every combination of 2 elements
-    for i in range(len(given)): # indexing by int to avoid comparing to self
-        for j in range(len(given[i:])):
-            i_val = given[i]
-            j_val = given[j]
-            if i_val + j_val == search:
+class ArraySumming:
+    @staticmethod
+    def sum_two_target(given: list, search: int)->bool:
+        # determine if there are any two = return early when a single match is found
+        # algorithm: add values to a set, then check while searching given list if target - current exists in set
+        my_set = set(given)
+        for element in given:
+            if search - element in my_set:
                 return True
-    return False
+        return False
 
-sum_two_target(given, search)
-sum_two_target_naive(given, search)
+    @staticmethod
+    def sum_two_target_naive(given: list, search: int)->bool:
+        # determine if there are any two = return early when a single match is found
+        # algorithm: check every combination of 2 elements
+        for i in range(len(given)): # indexing by int to avoid comparing to self
+            for j in range(i+1, len(given)):
+                i_val = given[i]
+                j_val = given[j]
+                if i_val + j_val == search:
+                    return True
+        return False
+
+
+if __name__ == "__main__":
+    given = [5, 7, 1, 2, 7, 4, 3]
+    search = 10
+    result_sum = ArraySumming.sum_two_target(given, search)
+    result_naive = ArraySumming.sum_two_target_naive(given, search)
+
+    print("{list} searching for {search} was found {sum}/{naive}".format(list=given, search=search, sum=result_sum,
+                                                                         naive = result_naive))
--- missing_number/__init__.py
+++ missing_number/__init__.py
@@ -1,2 +1,3 @@
 # __init__.py
 from missing_number.missing_number import MissingNumbers
+
