d4d592a83aef729f162602643ba4a0ede5b613ca
phasenoisepon <82134074+phasenoisepon@users.noreply.github.com>
2021-08-19 23:56:53 -0500

    Merge two Singly Linked Lists (#4)
    * initial commit
    
    * init py for SLLs
    
    * test cases
    
    * reshape main + tests
    
    * simplify code
    
    * final lints and tests

---
+++ merge_sorted_linked_list/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+from merge_sorted_linked_list.linkedlist import SingleLinkedList
+from merge_sorted_linked_list.linkedlist import Node
+from merge_sorted_linked_list.linkedlist import merge_sorted
+
+__all__ = ['SingleLinkedList', 'Node', 'merge_sorted', ]
